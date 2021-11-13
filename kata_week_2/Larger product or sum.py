587175bfdf885f1c7b00a75b5e5cb5281c9ec7ca
HAL9044 <diegomesamarrero@gmail.com>
2021-11-13 10:28:44 +0100

    kata terminada 2

--- kata_week_2/Larger product or sum.py
+++ kata_week_2/Larger product or sum.py
@@ -4,7 +4,7 @@ def sum_or_product(list, n):
 
     list.sort() #sort the list in order to operate
     suma = sum(list[-n:len(list)]) #sum biggest numbers starting from the end of the list
-    product = np.prod(list[n]) #multiply smallest numbers from the start of the list
+    product = np.prod(list[0:n]) #multiply smallest numbers from the start of the list
 
     #check whether sum is bigger or smaller than product, and return accordingly 
     
