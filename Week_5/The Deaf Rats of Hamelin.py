b950c6b4858c4996a2c8e8ab3dfe12de6fea62d1
HAL9044 <63170964+HAL9044@users.noreply.github.com>
2021-12-04 10:17:52 +0100

    Update The Deaf Rats of Hamelin.py

--- Week_5/The Deaf Rats of Hamelin.py
+++ Week_5/The Deaf Rats of Hamelin.py
@@ -2,13 +2,7 @@ def count_deaf_rats(town):
     der = 0
     izq = 0
     town = town.replace(' ', '')
-    print(town)
     ind = town.index("P")
-    print(ind)
     izq = town[1:ind:2].count("~")
-    der = town[8::2].count("O")
+    der = town[ind::2].count("O")
     return izq + der
-
-    
-
-
